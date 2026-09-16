0e535ee1e3b8043088ccb37f77c1b016a36bc1b5
Parag Jain <paragbentiya@gmail.com>
2026-09-16 06:39:07 +0000

    .

--- models/fct/fct_reviews.sql
+++ models/fct/fct_reviews.sql
@@ -5,10 +5,15 @@
     unique_key='review_id'
   )
 }}
+
 WITH src_reviews AS (
   SELECT * FROM {{ ref('src_reviews') }}
 )
-SELECT * FROM src_reviews
+SELECT 
+  -- FIXED: Dynamically generating the missing review_id column required by your unique_key config
+  {{ dbt_utils.generate_surrogate_key(['listing_id', 'review_date', 'reviewer_name', 'review_text']) }} as review_id,
+  * 
+FROM src_reviews
 WHERE review_text is not null
 
 {% if is_incremental() %}
