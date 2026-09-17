9f72c361eb7902b38e995485a682d7fbb8c4fe45
Parag Jain <paragbentiya@gmail.com>
2026-09-17 18:40:44 +0000

    dashboards

--- models/fct/fct_reviews.sql
+++ models/fct/fct_reviews.sql
@@ -1,21 +1,18 @@
 {{
   config(
-    materialized='incremental',
-    incremental_strategy='merge',
-    unique_key='review_id'
-  )
+    materialized = 'incremental',
+    on_schema_change='fail'
+    )
 }}
-
 WITH src_reviews AS (
   SELECT * FROM {{ ref('src_reviews') }}
 )
-SELECT 
-  -- FIXED: Dynamically generating the missing review_id column required by your unique_key config
-  {{ dbt_utils.generate_surrogate_key(['listing_id', 'review_date', 'reviewer_name', 'review_text']) }} as review_id,
-  * 
-FROM src_reviews
+SELECT
+  {{ dbt_utils.generate_surrogate_key(['listing_id', 'review_date', 'reviewer_name', 'review_text']) }}
+    AS review_id,
+  *
+  FROM src_reviews
 WHERE review_text is not null
-
 {% if is_incremental() %}
   AND review_date > (select max(review_date) from {{ this }})
 {% endif %}
